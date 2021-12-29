41592ec1946c6a0359327a7f026c6ea3279d05d6
Jorge Callalle <jlcallalle@gmail.com>
2021-12-29 16:44:31 -0500

    template movil - login

---
+++ app/js/main.js
@@ -0,0 +1,122 @@
+// Main
+
+var MyApp = {
+    toggeMenu : function () {
+        $('.button-nav--toggle').on('click', function(e) {
+            e.preventDefault();
+            $(this).toggleClass('active');
+        });
+    },
+}
+
+$(function () {
+    console.log('ready..');    
+    MyApp.toggeMenu();
+    // Mostrar panel clima grados
+    $('.texto-clima').on('click', function(e) {
+        e.preventDefault();
+        $('.mostrar-clima-detalle').toggleClass('active');
+    });
+
+    // Mostrar panel estado operacional
+    $('.event-clima').on('click', function(e) {
+        e.preventDefault();
+        $('.box-estado-clima').toggleClass('active');
+    });
+
+    // mostrar buscador
+    $('.item-lupa').on('click', function(e) {
+        e.preventDefault();
+        $('.mostrar-busqueda').toggleClass('active');
+    });
+
+    // mapa cerrar item 
+       $('.item-mapa button').on('click', function(e) {
+        e.preventDefault();
+        $(this).parent().addClass('hide');
+    });
+    
+
+    // mapa mostrar lugares de pago
+    $('.mostrar-lugares').on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $('.box-punto-pago').toggleClass('active');
+    });
+
+    // movil nav
+    $('.c-hamburger').on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('is-active');
+        $("#navigation").toggleClass("active");
+    });
+
+    $(".down-submenu-mobile").on("click", function () {
+        $(this).parent().toggleClass("active-submenu");
+    });
+
+   
+    //  menu hover overlay 
+    $( ".lista-nav li" ).hover(
+        function() {
+            $(this).parent().siblings().addClass('hover');
+        }, function() {
+            $(this).parent().siblings().removeClass('hover');
+        }
+    );
+
+    // mostrar listado noticias 
+     $('.listado-noticias > div').each(function(index) {
+        if (index >= 8) {
+            $(this).addClass('hide');
+        }
+    });
+
+    $('.btn-mostrar-noticias').on('click', function(e) {
+        e.preventDefault();
+        $('.listado-noticias .col-12').removeClass('hide');
+        $('.row-mostrar-noticias').hide();
+        $(this).hide();
+    });
+
+    // footer mostrar politicas
+    $('.lista-texto li').each(function(index) {
+        if (index >= 4) {
+            $(this).addClass('hide');
+        }
+    });
+
+    $('.btn-mostrar-politicas').on('click', function(e) {
+        e.preventDefault();
+        $('.lista-texto li').removeClass('hide');
+        $(this).hide();
+    });
+
+     // show hide input password
+    $('[data-toggle="password"]').each(function () {
+        var input = $(this);
+        var eye_btn = $(this).parent().find('.input-group-addon');
+        eye_btn.addClass('input-password-hide');
+        eye_btn.on('click', function (event) {
+            event.preventDefault(); 
+            if (eye_btn.hasClass('input-password-hide')) {
+                eye_btn.removeClass('input-password-hide').addClass('input-password-show');
+                eye_btn.find('.togglePassword').removeClass('fa-eye-slash').addClass('fa-eye')
+                 input.attr('type', 'text');
+            } else {
+                console.log('b');
+                eye_btn.removeClass('input-password-show').addClass('input-password-hide');
+                eye_btn.find('.togglePassword').removeClass('fa-eye').addClass('fa-eye-slash')
+                input.attr('type', 'password');
+            }
+        });
+    });
+
+    // active carrousel pago
+    $('.slider-pago li').on('click', function(e) {
+        $('.slider-pago li').removeClass('active');;
+        $(this).addClass('active');;
+    });
+
+
+});
